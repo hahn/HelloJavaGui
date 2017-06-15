68a752cf1040204c1e78957e81ae6272bd9a90b3
Hanhan <matcherapy@gmail.com>
2017-06-15 17:01:03 +0700

    first hello java gui

---
+++ src/hellojavagui/HelloJavaGui.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellojavagui;
+
+import hellojavagui.paketbaru.FrmHello;
+
+/**
+ *
+ * @author hahn
+ */
+public class HelloJavaGui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmHello hello = new FrmHello();
+        
+        hello.setVisible(true);
+    }
+    
+}
